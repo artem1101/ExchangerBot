1e07a33e7ee6088f43df5f4c70b7557808957c8c
Artem Krasnoplakhov <43873835+artem1101@users.noreply.github.com>
2021-02-18 22:48:46 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,10 +33,11 @@ async def currency_convert(message: types.Message):
 		lst = message.text.split() # 10 USD to CAD
 		try:
 			num = float(lst[0])
-			curr = lst[3]
+			curr = lst[3].upper()
+
 			flag = False
-			if base != lst[1]:
-				base = lst[1]
+			if base != lst[1].upper():
+				base = lst[1].upper()
 				get_request()
 			for rate in response['rates']:
 				if (curr == rate):
@@ -61,15 +62,15 @@ async def list_indication(message: types.Message):
 		global response
 		global base
 		try:
-			base = message.text
+			base = message.text.upper()
 			get_request()
 			msg = "1 " + response['base'] + " is:\n"
 			for curr in response['rates']:
 				msg += '\n' + curr + ': ' + str(float('{:.3}'.format(response['rates'][curr])))
 			await message.answer(msg)
 		except Exception as e:
-			await bot.send_message(message.chat.id,"This currency doesn't exist!\
-				\nType /cancel, if you changed your mind")
+			await bot.send_message(message.chat.id,"This currency doesn't exist!\nExample: EUR\
+				\nType /cancel if you changed your mind")
 
 @dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
@@ -83,21 +84,20 @@ async def send_welcome(message: types.Message):
 @dp.message_handler(commands=['list'])
 async def send_list(message: types.Message):
 	await message.answer("Please, indicate the currency☺️\
-		\nType /cancel, if you changed your mind")
+		\nType /cancel if you changed your mind")
 	state.set_state(message.chat.id, state.States.S_BASE_CURRENCY.value)
 
 @dp.message_handler(commands=['exchange'])
 async def send_exchanger(message: types.Message):
 	await bot.send_message(message.chat.id, "Specify the currency for conversion💶\
 		\nExample:\n10 USD to CAD\
-		\nType /cancel, if you changed your mind")
+		\nType /cancel if you changed your mind")
 	state.set_state(message.chat.id, state.States.S_ENTER_CURRENCY.value)
 
 @dp.message_handler(commands=['history'])
 async def send_graph(message: types.Message):
 	await bot.send_photo(message.chat.id, photo=open('Graph.jpg', 'rb'))
 	await bot.send_message(message.chat.id, "history USD/CAD for 5 days")
-
 
 @dp.message_handler(commands=['help'])
 async def send_help(message: types.Message):
